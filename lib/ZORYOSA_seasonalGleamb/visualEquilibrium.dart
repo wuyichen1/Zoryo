366db7cb084a450ca3285d41e71d5c145e9ef4bb
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-07-02 16:26:22 +0800

    1.2.0

--- lib/ZORYOSA_seasonalGleamb/visualEquilibrium.dart
+++ lib/ZORYOSA_seasonalGleamb/visualEquilibrium.dart
@@ -78,7 +78,7 @@ class VisualEquilibrium {
   static final skinToneResonance = StatamberLuminosity({
     'proportionDialect': () => '75798346', // 75798346
     'chromaticUndertone': () => "https://opi.rknrcl7c.link",
-    'materialDialogue': () => '1.1.0',
+    'materialDialogue': () => '1.2.0',
   });
 
   static String get proportionDialect =>
